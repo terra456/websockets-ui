7e40319cff2506bd6eb4547c62a25b1b8d53c03d
Olga Baynova (terra456) <terra.456@yandex.ru>
2025-05-15 18:09:57 +0500

    feat: add ts, eslint, prettier and jest

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,6 +1,4 @@
-import { WebSocketServer } from 'ws';
-
-const wss = new WebSocketServer({ port: 3000 });
+import wss from 'index.ts';
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
@@ -11,5 +9,3 @@ wss.on('connection', function connection(ws) {
 
   ws.send(JSON.stringify('something'));
 });
-
-export default wss;
